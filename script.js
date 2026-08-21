3e154a60d84bde5e91dd9aea3ea541b9423e7abf
mnaga9603-png <mnaga9603@gmail.com>
2026-08-21 08:12:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
 // ================= DATABASE (MOVIES + WEB SERIES + BIGG BOSS) =================
 const moviesDatabase = [
     {
+        id: 5,
+        type: "movie",
+        featured: true,
+        title: "Chennai Love Story",
+        year: "2026",
+        rating: "8.9",
+        quality: "1080P FHD",
+        category: "Netflix", // DVDRips / Netflix / Prime / Hotstar / Zee5
+        ott: "4K UHD",
+        language: "Telugu (Original)",
+        genres: "Action / Crime / Drama",
+        director: "Sukumar",
+        cast: "Allu Arjun, Rashmika Mandanna, Fahadh Faasil",
+        poster: "images/chennai.jpg",
+        backdrop: "images/chennai1",
+        trailerUrl: "https://www.youtube.com/embed/g3L556vA3Bw",
+        description: "Beautiful Movie to Watch This Weekend",
+        downloads: [
+            { quality: "480p SD - Mobile Quality", size: "~450 MB | x264", cloud: "https://ml-filetolink-botz-ddc645471993.herokuapp.com/85615?hash=AgAD9R", fast: "https://gofile.io/d/ICKmTT18" },
+            { quality: "720p HD - Standard Quality", size: "~1.2 GB | HEVC", cloud: "https://ml-filetolink-botz-ddc645471993.herokuapp.com/85619?hash=AgAD-R", fast: "https://gofile.io/d/9VxcIo9m" },
+            { quality: "1080p FHD - Full HD Quality", size: "~2.4 GB | 5.1 Audio", cloud: "https://ml-filetolink-botz-ddc645471993.herokuapp.com/85617?hash=AgAD_h", fast: "https://gofile.io/d/9GKiNRoI" }
+        ]
+    },
+    {
+        id: 4,
+        type: "movie",
+        featured: true,
+        title: "Srinivasa Mangapuram",
+        year: "2026",
+        rating: "8.9",
+        quality: "1080P FHD",
+        category: "Netflix", // DVDRips / Netflix / Prime / Hotstar / Zee5
+        ott: "Netflix",
+        language: "Telugu (Original)",
+        genres: "Action / Crime / Drama",
+        director: "Sukumar",
+        cast: "Allu Arjun, Rashmika Mandanna, Fahadh Faasil",
+        poster: "images/srinivas.jpg",
+        backdrop: "images/srinivas1",
+        trailerUrl: "https://www.youtube.com/embed/g3L556vA3Bw",
+        description: "Beautiful Movie to Watch This Weekend",
+        downloads: [
+            { quality: "480p SD - Mobile Quality", size: "~450 MB | x264", cloud: "https://ml-filetolink-botz-ddc645471993.herokuapp.com/85615?hash=AgAD9R", fast: "https://gofile.io/d/FvuzvXpC" },
+            { quality: "720p HD - Standard Quality", size: "~1.2 GB | HEVC", cloud: "https://ml-filetolink-botz-ddc645471993.herokuapp.com/85619?hash=AgAD-R", fast: "https://gofile.io/d/aTkrJgHW" },
+            { quality: "1080p FHD - Full HD Quality", size: "~2.4 GB | 5.1 Audio", cloud: "https://ml-filetolink-botz-ddc645471993.herokuapp.com/85617?hash=AgAD_h", fast: "https://gofile.io/d/zpfQVf6o" }
+        ]
+    },
+    {
         id: 1,
         type: "movie",
         featured: true,
